251660822767c33d846fedb80cac90df6f1f13f2
JakubRobo <robaszewskijakub@gmail.com>
2023-06-18 18:49:22 +0200

    Dodanie projektu

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -14,7 +14,7 @@ class StatisticsUtilsTest {
         )
         // given 
         val result = getActiveAndCompletedStats(tasks)
-        // then 
+        // then czy zwrocony = oczekiwany
         assertEquals(0f, result.completedTasksPercent)
         assertEquals(100f, result.activeTasksPercent)
     }
